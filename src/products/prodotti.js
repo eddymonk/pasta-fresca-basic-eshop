0a5440f144a8d1b27a735c049c7ff1ab4f0b573e
Edoardo Monaco <edoardo.monaco@creditinfo.com>
2024-04-14 14:41:14 +0200

    f

--- src/products/prodotti.js
+++ src/products/prodotti.js
@@ -2,12 +2,12 @@ export const productObj = {
   products: [
     {
       id: 1,
-      title: "Tortellini con carne",
+      title: "Ravioli con carne",
       description: "Tortellini fatti a mano ripieni di carne mista",
       price: 8,
       stock: 94,
       brand: "Casagrande pastificio",
-      category: "Tortellini",
+      category: "Ravioli",
       thumbnail:
         "https://upload.wikimedia.org/wikipedia/commons/6/63/Fresh_tortellini.jpg",
       images: [
@@ -16,12 +16,12 @@ export const productObj = {
     },
     {
       id: 2,
-      title: "Tagliatelle",
+      title: "Tortellini con carne",
       description: "Tagliatelle fatte e mano",
       price: 5,
       stock: 94,
       brand: "Casagrande pastificio",
-      category: "Pasta Fresca",
+      category: "Tortellini",
       thumbnail:
         "https://upload.wikimedia.org/wikipedia/commons/6/63/Fresh_tortellini.jpg",
 
@@ -31,12 +31,12 @@ export const productObj = {
     },
     {
       id: 3,
-      title: "Ravioli con merda",
+      title: "Tortellini con carne",
       description: "Tortellini fatti a mano ripieni di merda mista",
       price: 11,
       stock: 94,
       brand: "Casagrande pastificio",
-      category: "Ravioli",
+      category: "Pasta Ripiena",
       thumbnail:
         "https://static.wixstatic.com/media/119e37_59f6e3a06e314489a8abaf3b546e2f37~mv2.jpg/v1/fill/w_450,h_450,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/119e37_59f6e3a06e314489a8abaf3b546e2f37~mv2.jpg",
       images: [
@@ -45,12 +45,12 @@ export const productObj = {
     },
     {
       id: 4,
-      title: "Tortellini con pesce",
+      title: "Tagliatelle",
       description: "Tortellini fatti a mano ripieni di pesce mista",
       price: 10,
       stock: 94,
       brand: "Casagrande pastificio",
-      category: "Tortellini",
+      category: "Pasta Lunga",
       thumbnail:
         "https://upload.wikimedia.org/wikipedia/commons/6/63/Fresh_tortellini.jpg",
       images: [
@@ -59,12 +59,12 @@ export const productObj = {
     },
     {
       id: 5,
-      title: "Tortellini con spinaci",
+      title: "Pennette",
       description: "Tortellini fatti a mano ripieni di spinaci",
       price: 7,
       stock: 94,
       brand: "Casagrande pastificio",
-      category: "Tortellini",
+      category: "Pasta Corta",
       thumbnail:
         "https://upload.wikimedia.org/wikipedia/commons/6/63/Fresh_tortellini.jpg",
       images: [
@@ -73,60 +73,17 @@ export const productObj = {
     },
     {
       id: 6,
-      title: "Fettuccine",
+      title: "Olive all'ascolana",
       description: "Fettuccine fatte e mano",
       price: 5,
       stock: 94,
       brand: "Casagrande pastificio",
-      category: "Pasta Fresca",
+      category: "Specialita",
       thumbnail:
         "https://upload.wikimedia.org/wikipedia/commons/6/63/Fresh_tortellini.jpg",
 
       images: [
         "https://upload.wikimedia.org/wikipedia/commons/6/63/Fresh_tortellini.jpg",
-      ],
-    },
-    {
-      id: 7,
-      title: "Rigatoni",
-      description: "Rigatoni fatte e mano",
-      price: 4,
-      stock: 94,
-      brand: "Casagrande pastificio",
-      category: "Pasta Fresca",
-      thumbnail:
-        "https://upload.wikimedia.org/wikipedia/commons/6/63/Fresh_tortellini.jpg",
-
-      images: [
-        "https://upload.wikimedia.org/wikipedia/commons/6/63/Fresh_tortellini.jpg",
-      ],
-    },
-    {
-      id: 8,
-      title: "Ravioli con Hash",
-      description: "Ravioli fatti a mano ripieni di merda mista",
-      price: 11,
-      stock: 94,
-      brand: "Casagrande pastificio",
-      category: "Ravioli",
-      thumbnail:
-        "https://static.wixstatic.com/media/119e37_59f6e3a06e314489a8abaf3b546e2f37~mv2.jpg/v1/fill/w_450,h_450,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/119e37_59f6e3a06e314489a8abaf3b546e2f37~mv2.jpg",
-      images: [
-        "https://static.wixstatic.com/media/119e37_59f6e3a06e314489a8abaf3b546e2f37~mv2.jpg/v1/fill/w_450,h_450,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/119e37_59f6e3a06e314489a8abaf3b546e2f37~mv2.jpg",
-      ],
-    },
-    {
-      id: 9,
-      title: "Ravioli con THC",
-      description: "Ravioli fatti a mano ripieni di merda mista",
-      price: 11,
-      stock: 94,
-      brand: "Casagrande pastificio",
-      category: "Ravioli",
-      thumbnail:
-        "https://static.wixstatic.com/media/119e37_59f6e3a06e314489a8abaf3b546e2f37~mv2.jpg/v1/fill/w_450,h_450,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/119e37_59f6e3a06e314489a8abaf3b546e2f37~mv2.jpg",
-      images: [
-        "https://static.wixstatic.com/media/119e37_59f6e3a06e314489a8abaf3b546e2f37~mv2.jpg/v1/fill/w_450,h_450,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/119e37_59f6e3a06e314489a8abaf3b546e2f37~mv2.jpg",
       ],
     },
   ],
